5577fa6d99af2c9fd26c66743e4490d097bba8fe
elias.santos <elias.carvalho.096@gmail.com>
2019-12-13 00:19:19 -0300

    ViewModel INotifyPropertyChanged

---
+++ ShellXamarinForms/ShellXamarinForms/ViewModel/PrimatasDetalhesViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using ShellXamarinForms.Model;
+using ShellXamarinForms.Data;
+
+namespace ShellXamarinForms.ViewModel
+{
+    [QueryProperty("NomePrimata", "nome")]
+    public class PrimatasDetalhesViewModel : INotifyPropertyChanged
+    {
+        public string NomePrimata
+        {
+            set
+            {
+                Animal primata = PrimatasData.Primatas.FirstOrDefault(p => p.Nome == Uri.UnescapeDataString(value));
+
+                if(primata != null)
+                {
+                    Nome = primata.Nome;
+                    Localizacao = primata.Localizacao;
+                    Detalhes = primata.Detalhes;
+                    UrlImagem = primata.UrlImagem;
+                    OnPropertyChanged("Nome");
+                    OnPropertyChanged("Localizacao");
+                    OnPropertyChanged("Detalhes");
+                    OnPropertyChanged("UrlImagem");
+                }
+            }
+        }
+
+        public string Nome { get; set; }
+        public string Localizacao { get; set; }
+        public string Detalhes { get; set; }
+        public string UrlImagem { get; set; }
+
+        #region "INotifyPropertyChanged"
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propNome = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propNome));
+        }
+        
+        #endregion
+
+    }
+}
---
+++ ShellXamarinForms/ShellXamarinForms/ViewModel/UrsosViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Linq;
+using System.Collections.ObjectModel;
+using ShellXamarinForms.Model;
+using ShellXamarinForms.Data;
+
+namespace ShellXamarinForms.ViewModel
+{
+    public class UrsosViewModel
+    {
+        public ObservableCollection<Animal> ResultadosBusca { get; private set; }
+        public ICommand SearchCommand => new Command<string>(ItemsBusca);
+
+        void ItemsBusca(string query)
+        {
+            if(string.IsNullOrWhiteSpace(query))
+            {
+                ResultadosBusca = null;
+            }
+            else
+            {
+                var itemsFiltrados = UrsosData.Ursos
+                    .Where(urso => urso.Nome.ToLower().Contains(query.ToLower()))
+                    .ToList();
+                ResultadosBusca = new ObservableCollection<Animal>(itemsFiltrados);
+            }
+        }
+
+    }
+}
