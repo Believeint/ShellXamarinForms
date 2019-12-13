e21ff4346591fc84b5d5bfe633633a66e78ab38e
elias.santos <elias.carvalho.096@gmail.com>
2019-12-12 22:23:19 -0300

    Initial Commit/ AppShell & Ambiente

---
+++ ShellXamarinForms/ShellXamarinForms/Control/ManipuladorPesquisaAnimal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ShellXamarinForms.Control
+{
+    public class ManipuladorPesquisaAnimal : SearchHandler
+    {
+        
+
+    }
+}
---
+++ ShellXamarinForms/ShellXamarinForms/Control/SubMenu.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShellXamarinForms.Control
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SubMenu : ContentView
+    {
+        public SubMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
